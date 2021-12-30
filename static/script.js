5ffca8346c6a67276d7ac37a3bef487befd56a80
Aleksei222 <2006alpsh@gmail.com>
2021-12-30 08:54:00 +0300

    rewrote into Vue

--- static/script.js
+++ static/script.js
@@ -1,30 +1,42 @@
-function inputInt(x){
-    document.getElementById("input").value += x;
-}
-function sbros(){
-    document.getElementById("input").value = "";
-}
-function dl(){
-   a =  document.getElementById("input").value
-   document.getElementById("input").value = a.slice(0, -1) 
-}
-function delIncorrect(){
-    if (document.getElementById("input").value == "incorrect"){
-        document.getElementById("input").value = ""
-    } 
+const btns = ".+123-456/789*(0)"
+
+const Key = {
+    props: ['value'],
+    template: `<button type = 'button' class = 'btn'>{{value}}</button>`
 }
 
-async function calc(e) {
-    e.preventDefault();
-    const input = document.getElementById('input');
-    const response = await fetch('/api/calc/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
+const app = Vue.createApp({
+    data(){
+        return {
+            keys: btns,
+            exp: '',
+            K: '<'
+        }
+    },
+    methods: {
+        Sbros(){
+            this.exp = ''
         },
-        body: JSON.stringify({expression: input.value})
-    });
-    const result = await response.json();
-    input.placeholder = result.placeholder;
-    input.value = result.value;
-}
+        Dl(){
+            this.exp = this.exp.slice(0, -1)
+        },
+        async calc(e){
+            e.preventDefault();
+            const input = document.getElementById('input');
+            const response = await fetch('/api/calc/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({expression: input.value})
+            });
+            const result = await response.json();
+            input.placeholder = result.placeholder;
+            this.exp = result.value;
+        }
+    },
+    components: {
+      Key
+    },
+})
+app.mount('#calc')
